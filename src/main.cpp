cc08d2d63ed595ca32e1617d9ba2a93a2d3badc6
KASHISH GUPTA <kashish.gupta_cs.aiml24@gla.ac.in>
2026-02-28 16:04:32 +0530

    Initialize project with documentation and Arduino include

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,16 @@
 #include <Arduino.h>
+
 /**
  * @file main.ino
  * @brief Embedded Temperature and Humidity Monitoring using DHT11
- * @author YOUR_NAME
- * @date YYYY-MM-DD
+ * @author Kashish Gupta
+ * @date 2026-02-17
  *
  * @details
  * This program reads environmental data from the DHT11 sensor
  * and displays temperature and humidity values on Serial Monitor.
- * Students must complete the TODO sections.
  */
+
 
 #include <DHT.h>
 
